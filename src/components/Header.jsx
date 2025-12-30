341242ae7fa05deb3cfdaffd08f6c98f4fc1e2d5
GitHub Deployer <deployer@github.com>
2025-12-30 16:17:56 +0800

    Fix mobile menu overlay: move outside header container to fix z-index stacking context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,6 +41,7 @@ const Header = () => {
     }, [location.pathname]);
 
     return (
+        <>
         <header
             className={cn(
                 'fixed top-0 left-0 right-0 z-50 transition-all duration-300',
@@ -149,6 +150,7 @@ const Header = () => {
                 </AnimatePresence>
             </div>
         </header>
+        </>
     );
 };
 

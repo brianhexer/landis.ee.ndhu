7ea6ec9f0c7b13f269fa4e9870c3f306be66f603
GitHub Deployer <deployer@github.com>
2025-12-30 20:03:00 +0800

    Revert custom domain configuration, restore GitHub Pages subdirectory setup

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import ScrollToTop from '@/components/ScrollToTop';
 import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/landis">
         <ScrollToTop />
         <App />
     </BrowserRouter>

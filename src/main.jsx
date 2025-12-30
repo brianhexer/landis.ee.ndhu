5a962ed895b02dbb6053a7ac9e45340223f6165b
GitHub Deployer <deployer@github.com>
2025-12-30 15:06:57 +0800

    Fix routing: add basename '/landis' to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,8 @@ import ScrollToTop from '@/components/ScrollToTop';
 import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
-        <BrowserRouter>
-            <ScrollToTop />
-            <App />
-        </BrowserRouter>
-    </>
+    <BrowserRouter basename="/landis">
+        <ScrollToTop />
+        <App />
+    </BrowserRouter>
 );

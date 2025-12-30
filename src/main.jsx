b7fc2b5f24ddb88a7a3fa6130f3f95ad6988eca9
GitHub Deployer <deployer@github.com>
2025-12-30 20:14:28 +0800

    Update base path for landis.ee.ndhu repository

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import ScrollToTop from '@/components/ScrollToTop';
 import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename="/landis">
+    <BrowserRouter basename="/landis.ee.ndhu">
         <ScrollToTop />
         <App />
     </BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/landis/',
+    base: '/landis.ee.ndhu/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),

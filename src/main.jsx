ad0b2a4e2e8182ffbb6c4e9abe8872c3ec81c085
GitHub Deployer <deployer@github.com>
2025-12-30 19:30:58 +0800

    Configure custom domain www.landis.ee.ndhu.edu.tw

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import ScrollToTop from '@/components/ScrollToTop';
 import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename="/landis">
+    <BrowserRouter>
         <ScrollToTop />
         <App />
     </BrowserRouter>
